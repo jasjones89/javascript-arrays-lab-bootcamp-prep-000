fb0cc2577b477de2818b3a11f2cc773deb6c89f2
jasjones89 <zebla9@gmail.com>
2017-10-13 01:34:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,14 +27,14 @@ return [...kittens, name];
 function prependKitten(name){
 
 return [name,...kittens];
-  
+
 }
 function removeLastKitten() {
    return kittens.slice(0, kittens.length - 1);
-  
+
 }
 
 function removeFirstKitten() {
 return  kittens= kittens.slice(1);
-  
+
 }
